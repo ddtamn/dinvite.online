2a6a11cd3213d922e5ac22bc16e6e750c0bd5f8b
diditamin.m <disanderlidsbun@gmail.com>
2022-07-04 20:00:55 +0800

    add global css cano font & favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,37 @@
+<script>
+    import '../css/global.css'
+</script>
+<svelte:head>
+    <title>dinvite</title>
+</svelte:head>
 <header>
     <div class="container">
         <a href="/" class="logo">dinvite</a>
         <div class="mobile-toggle"><i class="fa-solid fa-bars"></i></div>
         <div class="close-toggle"><i class="fa-solid fa-xmark"></i></div>
     </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/">Theme</a>
+            </li>
+            <li>
+                <a href="/">Pricelist</a>
+            </li>
+            <li>
+                <a href="/">Blog</a>
+            </li>
+        </ul>
+    </nav>
 </header>
+
+
+<style>
+   
+</style>
