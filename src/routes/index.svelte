9c2a32afd14149e3802673c49f1dc285a31032c6
diditamin.m <disanderlidsbun@gmail.com>
2022-07-05 00:39:19 +0800

    add theme color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script>
+    import { onMount } from 'svelte';
     import '../css/style.css'
     import Header from '../components/Header.svelte';
     import Hero from '../components/Hero.svelte';
     import Wave from '../components/Wave.svelte';
-    import { onMount } from 'svelte';
-import About from '../components/About.svelte';
-import Features from '../components/Features.svelte';
-import Pricelist from '../components/Pricelist.svelte';
+    import About from '../components/About.svelte';
+    import Features from '../components/Features.svelte';
+    import Pricelist from '../components/Pricelist.svelte';
 
     onMount(() => {
         document.body.style.overflowY = 'scroll'
@@ -14,6 +14,7 @@ import Pricelist from '../components/Pricelist.svelte';
 </script>
 
 <svelte:head>
+    <meta name="theme-color" content="#5a9fbf">
     <title>Dinvite</title>
 </svelte:head>
 
