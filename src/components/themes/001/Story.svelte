9ee74a5291f0301a045943f6a6920e5af97ea80a
diditamin.m <disanderlidsbun@gmail.com>
2022-07-13 23:56:28 +0800

    add love story section for theme 001

---
+++ src/components/themes/001/Story.svelte
@@ -0,0 +1,127 @@
+<section>
+    <div class="container">
+        <div class="head" data-aos="fade-up">
+            <h3>Love Story</h3>
+            <p>Marriage is the golden ring in a chain whose beginning is a glance and whose ending is eternity</p>
+        </div>
+        <div class="timeline" data-aos="fade-down">
+            <div class="inner" data-aos="fade-down">
+                <div class="content" data-aos="fade-right">
+                    <h4>First Meet</h4>
+                    <p>Kisah ini berawal dari pertemuan kita di bulan mei tahun 2018.</p>
+                </div>
+                <div class="content" data-aos="fade-right">
+                    <h4>Relationship​</h4>
+                    <p>Setelah menjalani hubungan selama 3 tahun, kita berkomitmen untuk masa depan hubungan kita.</p>
+                </div>
+                <div class="content" data-aos="fade-right">
+                    <h4>Engagement​​</h4>
+                    <p>Pada tgl 02 Januari 2021 Andi memberanikan diri membawa keluarganya bersilaturahmi ke rumah saya, dan meminta saya untuk menikah dengannya.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<style>
+    .container {
+        padding: 1rem;
+    }
+
+    .head {
+        text-align: center;
+        color: var(--primary-color);
+    }
+
+    h3 {
+        font-family: 'Arima', cursive;
+        font-weight: 900;
+        
+    }
+
+    .timeline {
+        position: relative;
+    }
+
+    .timeline::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 3px;
+        background-color: darkgrey;
+    }
+
+    .inner {
+        position: relative;
+        padding-block: 2rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        width: 100%;
+        padding-left: 2rem;
+        color: whitesmoke;
+    }
+
+    .inner::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: -6px;
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: darkgrey;
+    }
+    .inner::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: -6px;
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: darkgrey;
+    }
+
+    .content {
+        background: var(--primary-color);
+        padding: 1rem;
+        border-radius: 00.5rem;
+        position: relative;
+        box-shadow: var(--box-shadow);
+        border-left: 1px solid var(--secondary-color);
+        
+    }
+
+    .content::before {
+        content: '❤';
+        position: absolute;
+        left: -45px;
+        top: 12px;
+        height: 30px;
+        width: 30px;
+        background-color: var(--primary-color);
+        text-align: center;
+        display: grid;
+        place-items: center;
+        border-radius: 50%;
+    }
+
+    .content::after {
+        content: '';
+        display: block;
+        position: absolute;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 9px 9px 9px 0;
+        left: 0;
+        top: 18px;
+        margin-left: -9px;
+        border-right-color: var(--secondary-color)
+
+    }
+</style>
--- src/routes/tema/preview/001.svelte
+++ src/routes/tema/preview/001.svelte
@@ -7,6 +7,9 @@
     import SaveTheDate from '../../../components/themes/001/SaveTheDate.svelte';
     import Gallery from '../../../components/themes/001/Gallery.svelte';
     import Events from '../../../components/themes/001/Events.svelte';
+    import Story from '../../../components/themes/001/Story.svelte';
+    import Wish from '../../../components/themes/001/Wish.svelte';
+
     import AOS from 'aos'
     import "aos/dist/aos.css";
     import { onMount } from 'svelte';
@@ -27,6 +30,8 @@
     <Hero/>
     <Brides/>
     <SaveTheDate/>
+    <Story/>
     <Gallery/>
     <Events/>
+    <Wish/>
 </main>
