27d132d4fa7c8f589871503728bbe6ae02ddb3f6
diditamin.m <disanderlidsbun@gmail.com>
2022-08-11 14:23:13 +0800

    seo image change resolution

--- src/routes/ayu-aldhi.svelte
+++ src/routes/ayu-aldhi.svelte
@@ -73,7 +73,7 @@
     <meta property="og:url" content="https://dinvite.online/ayu-aldhi">
     <meta property="og:title" content="The Wedding of Ayu & Aldhi">
     <meta property="og:description" content="You are invited to the wedding of ayu & aldhi">
-    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="og:image" content="https://dinvite.online/uploads/ayualdi.png">
     <meta name="theme-color" content="#3f5978"/>
 
 </svelte:head>
