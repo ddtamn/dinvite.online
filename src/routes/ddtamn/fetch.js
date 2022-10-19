17f64126a6668462f044a62f6d0326aff7b91ba3
diditamin.m <disanderlidsbun@gmail.com>
2022-10-19 17:37:40 +0800

    change hero img

--- src/routes/ddtamn/fetch.js
+++
@@ -1,6 +0,0 @@
-// export const request = async (path, method, body = {}) => {
-// 	return await fetch(path, {
-// 		method,
-// 		body: JSON.stringify(body)
-// 	});
-// };
--- src/routes/ddtamn/firebase-admin.js
+++
@@ -1,8 +0,0 @@
-// import admin from 'firebase-admin';
-// import * as serviceAccount from './dinvite-database-firebase-adminsdk-c0xro-04d9496605.json';
-
-// admin.initializeApp({
-// 	credential: admin.credential.cert(serviceAccount)
-// });
-
-// export default admin;
